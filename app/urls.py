d75670f0806838f2898ca2f9b32524ec38d57e25
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2020-10-27 23:07:07 +0900

    feat(start-app) add start up 1 and 2

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    # path('polls', include('polls.url'))
+    path('polls/', include('polls.urls')),
 ]
